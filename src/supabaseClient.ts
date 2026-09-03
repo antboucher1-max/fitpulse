16e36ee983674468e5515c6f38ecef64b4ce4e7b
antboucher1-max <antboucher1@gmail.com>
2026-09-03 14:22:10 +0200

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://obtahwmcoqrcauscpksv.supabase.co';
-const supabaseAnonKey = 'sb_publishable_O8CKhUtzgq9nO9lKavNE9A__fAdRWoB';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9idGFod21jb3FyY2F1c2Nwa3N2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc3MDQ3OTQsImV4cCI6MjEwMzI4MDc5NH0.TyDAdy9XWFAtOHBLH9-9z6K-SSwSDTkaa3cJsLVQxro';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
